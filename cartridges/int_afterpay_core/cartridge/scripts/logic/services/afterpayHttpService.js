fb69b329a90372dd52105ce5f46aefab58eda9d2
afterpayplugins <62083833+ghatamehta-afterpay@users.noreply.github.com>
2024-02-13 14:53:13 +1100

    Updated version

--- cartridges/int_afterpay_core/cartridge/scripts/logic/services/afterpayHttpService.js
+++ cartridges/int_afterpay_core/cartridge/scripts/logic/services/afterpayHttpService.js
@@ -49,7 +49,7 @@ function getAfterpayHttpService() {
             service.setRequestMethod(requestBody.requestMethod);
             service.addHeader('Content-Type', 'application/json');
 
-            var afterpayCartridge = 'AfterpayCartridge/24.1.0';
+            var afterpayCartridge = 'AfterpayCartridge/24.1.1';
             var merchantID = service.configuration.credential.user;
             var siteURL = URLUtils.httpsHome().toString();
             var storeFront = Site.getCurrent().getID();
